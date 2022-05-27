5d3aa64a858c4ceef9bbd6acd025616c8239782c
Ashen-den <97136128+Ashen-den@users.noreply.github.com>
2022-05-26 22:03:08 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.querySelector('#getRocked').addEventListener('click', makeReq)
+
+
+async function makeReq(){
+
+  const userName = document.querySelector("#playGame").value;
+  const res = await fetch(`/api?student=${userName}`)
+  const data = await res.json()
+
+  console.log(data);
+  document.querySelector("#personName").textContent = data.name
+  document.querySelector("#personStatus").textContent = data.status
+  document.querySelector("#personOccupation").textContent = data.currentOccupation
+} /*hello*/
