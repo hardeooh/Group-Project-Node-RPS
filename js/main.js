8fb49c0f74a80d4a204577523c34f18624e3a60b
Ashen-den <97136128+Ashen-den@users.noreply.github.com>
2022-05-26 23:07:17 -0700

    Fixed logic on functions in main.js

--- js/main.js
+++ js/main.js
@@ -3,10 +3,9 @@ document.querySelector('#getRocked').addEventListener('click', makeReq)
 
 async function makeReq(){
   const userPick = document.querySelector("#playGame").value;
-  console.log(userPick)
   const res = await fetch(`/api`)
   const data = await res.json()
-  const checkWinner = calculateWin(userPick,data.result)
+  const checkWinner = [verifyUserChoice(userPick), calculateWin(userPick,data.result)]
   renderResults(checkWinner)
   console.log(data.result, userPick, checkWinner)
 }
@@ -18,6 +17,21 @@ function calculateWin(userChoice, computerChoice) {
   else return "You Lose!";
 }
 
+//Check if error message exist in index 0, if not render winner in index 1
 function renderResults(winner){
-  document.querySelector('#renderResult').textContent = winner
+  if(winner[0]){
+    document.querySelector('#renderResult').textContent = winner[0]
+  } else {
+    document.querySelector('#renderResult').textContent = winner[1]
+  }
+}
+
+//Check if user has inputted a valid value
+function verifyUserChoice(userValue){
+  const userValueToLowerCase = userValue.toLowerCase().trim()
+  if (userValueToLowerCase==='rock' || userValueToLowerCase==='paper' || userValueToLowerCase==='scissor'){
+    return ""
+  } else {
+    return 'You must pick rock, paper, or scissor'
+  }
 }
