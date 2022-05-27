318ecb77c2fee78ea6bc73db974871bf8690e6ac
Ashen-den <97136128+Ashen-den@users.noreply.github.com>
2022-05-26 22:14:44 -0700

    cleaned up server.js

--- js/main.js
+++ js/main.js
@@ -2,13 +2,17 @@ document.querySelector('#getRocked').addEventListener('click', makeReq)
 
 
 async function makeReq(){
-
-  const userName = document.querySelector("#playGame").value;
-  const res = await fetch(`/api?student=${userName}`)
+  const userPick = document.querySelector("#playGame").value;
+  console.log(userPick)
+  const res = await fetch(`/api?choice=${userPick}`)
   const data = await res.json()
+  calculateWin(userPick,data.result)
+  console.log(data.result);
+}
 
-  console.log(data);
-  document.querySelector("#personName").textContent = data.name
-  document.querySelector("#personStatus").textContent = data.status
-  document.querySelector("#personOccupation").textContent = data.currentOccupation
-} /*hello*/
+//Ciru Function to calculate winner
+function calculateWin(userChoice, computerChoice) {
+  if ((userChoice === 'rock' && computerChoice === 'scissors') || (userChoice === 'paper' && computerChoice === 'rock') || (userChoice === 'scissors' && computerChoice === 'paper')) return 'You win!';
+  else if (userChoice === computerChoice) return "It's a tie!";
+  else return "You Lose!";
+}
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs')
 const url = require('url');
 const querystring = require('querystring');
-const figlet = require('figlet')
+const figlet = require('figlet');
 
 const server = http.createServer((req, res) => {
   const page = url.parse(req.url).pathname;
@@ -21,8 +21,8 @@ const server = http.createServer((req, res) => {
         return choices[Math.floor(Math.random() * 3)];
       }
       const computerChoice = {
-      result: returnComputerChoice();
-      };
+        result: returnComputerChoice()
+      }
       res.end(JSON.stringify(computerChoice));
   } else if(params['student'] != 'leon'){
       res.writeHead(200, {'Content-Type': 'application/json'});
@@ -32,21 +32,18 @@ const server = http.createServer((req, res) => {
         currentOccupation: "unknown"
         }
         res.end(JSON.stringify(objToJson));
-      }//student != leon
-    }//student if
-  }//else if
-  else if (page == '/css/style.css'){
+  } else if (page == '/css/style.css'){
     fs.readFile('css/style.css', function(err, data) {
       res.write(data);
       res.end();
     });
-  }else if (page == '/js/main.js'){
+  } else if (page == '/js/main.js'){
     fs.readFile('js/main.js', function(err, data) {
       res.writeHead(200, {'Content-Type': 'text/javascript'});
       res.write(data);
       res.end();
     });
-  }else{
+  } else {
     figlet('404!!', function(err, data) {
       if (err) {
           console.log('Something went wrong...');
